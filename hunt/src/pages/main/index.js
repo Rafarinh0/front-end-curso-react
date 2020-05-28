dd2262f3ecf8367b666158d7dc68a97011df5284
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-05-28 16:47:15 -0300

    Armazenamento de 'variáveis' no state

--- hunt/src/pages/main/index.js
+++ hunt/src/pages/main/index.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 export default class Main extends Component{
+    state = {
+        products: [],
+    }
+    //Sempre que tivermos uma variável num state, o método
+    //render vai ouvir qualquer alteração de qualquer variável
+    //e vai renderizar essas alterações em tela
+
     componentDidMount(){
         this.loadProducts();
     }
@@ -9,11 +16,16 @@ export default class Main extends Component{
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        console.log(response.data.docs);
-    };
+        this.setState({ products: response.data.docs })};
 // quando a função é criada por mim e não pelo react,
 // eu preciso usar arrow function!
     render(){
-        return <h1>Hello World</h1>;
+        return(
+            <div className="product-list">
+                {this.state.products.map(product =>(
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        )
     }
 }
