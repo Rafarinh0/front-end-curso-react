8285c92536cc2d120eee618d09a8ceb10d3a5e81
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-05-29 21:19:17 -0300

    Listagem de produtos e estilização dos itens da página

--- hunt/src/pages/main/index.js
+++ hunt/src/pages/main/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import './styles.css';
 
-export default class Main extends Component{
+export default class Main extends Component {
     state = {
         products: [],
     }
@@ -9,21 +10,29 @@ export default class Main extends Component{
     //render vai ouvir qualquer alteração de qualquer variável
     //e vai renderizar essas alterações em tela
 
-    componentDidMount(){
+    componentDidMount() {
         this.loadProducts();
     }
 
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        this.setState({ products: response.data.docs })};
-// quando a função é criada por mim e não pelo react,
-// eu preciso usar arrow function!
-    render(){
-        return(
+        this.setState({ products: response.data.docs })
+    };
+    // quando a função é criada por mim e não pelo react,
+    // eu preciso usar arrow function!
+    render() {
+        const {products} = this.state;
+
+        return (
             <div className="product-list">
-                {this.state.products.map(product =>(
-                    <h2 key={product._id}>{product.title}</h2>
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="">Acessar</a>
+                    </article>
                 ))}
             </div>
         )
